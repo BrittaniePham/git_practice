c1c3e9f40b27f5eeea8feda974a1c625570544a2
Brittanie Pham <brittaniepham@gmail.com>
2018-05-10 12:03:44 -0600

    move git helper to own module

--- main.rb
+++ main.rb
@@ -1,26 +1,27 @@
 require 'colorize'
+require_relative 'git'
 
-def puts_git(cmd)
-  puts `git #{cmd} -h`
-end
 
-def menu
-  puts 'MAIN MENU'.colorize(:cyan)
-  puts '1: Enter git command'.colorize(:cyan)
-  puts '2: Exit'.colorize(:cyan)
-  choice = gets.to_i
+class Main
+  include Git
+  def self.menu
+    puts 'MAIN MENU'.colorize(:cyan)
+    puts '1: Enter git command'.colorize(:cyan)
+    puts '2: Exit'.colorize(:cyan)
+    choice = gets.to_i
 
-  case choice
-    when 1 
-      puts 'Enter a git command'.colorize(:green)
-      puts_git(gets.strip)
-      menu
-    when 2
-      exit
-    else
-      puts 'Invalid Choice'.colorize(:red)
-      menu
+    case choice
+      when 1 
+        puts 'Enter a git command'.colorize(:green)
+        Git.puts_git(gets.strip)
+        menu
+      when 2
+        exit
+      else
+        puts 'Invalid Choice'.colorize(:red)
+        menu
+    end
   end
 end
 
-menu
+Main.menu
